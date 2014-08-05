966dbced7d86293cd5a1dde2734094f0c22424bc
stefceror <stefsnow@gmail.com>
2014-08-04 19:48:08 -0700

    adds event listeners, input forms, send functionality, and fixes username issues

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,16 +5,23 @@ var app = {
   lastMessageID: undefined
 };
 
+var user = {
+  username: location.search.split("username=")[1],
+  room: "lobby",
+  friendsList: {}
+};
+
 //fetch initial messages, display them, kickoff fetch loop
 app.init = function(){
-  this.fetch();
-
+  $(document).ready(function(){
+    app.fetch();
+  });
 };
 
 app.send = function(message){
   //send a message
   $.ajax({
-    url: this.server,
+    url: app.server,
     type: "POST",
     data: JSON.stringify(message),
     dataType:"json",
@@ -31,42 +38,61 @@ app.send = function(message){
 app.fetch = function(){
   //submit a get request via ajax
   $.ajax({
-    url: this.server,
+    url: app.server,
     type: "GET",
     contentType: "jsonp",
     data: { order: "-createdAt" },
-    success: this.addNewestMessages.bind(this),
+    success: app.addNewestMessages,
     failure: function(data) {
       console.log("fetch failed");
     }
   });
 };
 
+// app.afterGET = function(data) {
+//   app.addNewestMessages(data);
+//   app.addEventListeners;
+// };
+
+app.addEventListeners = function() {
+  $(".username").on("click", function(){
+    var userName = $(this).text();
+    app.addFriend(userName);
+  });
+};
+
 app.findLastMessageIndex = function(messages){
   var index = 0;
   for(var i = 0; i < messages.length; i++){
-    if(messages[i].objectId === this.lastMessageID){
+    if(messages[i].objectId === app.lastMessageID){
       index = i;
       break;
     }
   }
   return index;
-}
+};
 
+//TODO: handle escaping
 app.addNewestMessages = function(data) {
-  console.log(data);
   var messages = data.results;
-  var lastIndex = this.initialMessageCount;
+  var lastIndex = app.initialMessageCount;
 
-  if (this.lastMessageID) {
-    lastIndex = this.findLastMessageIndex(messages);
+  // check if messages have been added before
+  if (app.lastMessageID) {
+    // update index of last message
+    lastIndex = app.findLastMessageIndex(messages);
   }
 
+  // iterate over new messages to prepend to DOM
   for (var i = lastIndex - 1; i >= 0; i--) {
-    this.addMessage(messages[i]);
+    app.addMessage(messages[i]);
   }
 
-  this.lastMessageID = messages[0].objectId;
+  // update last message
+  app.lastMessageID = messages[0].objectId;
+
+  // add event listeners
+  app.addEventListeners();
 }
 
 app.addMessage = function(message){
@@ -85,6 +111,25 @@ app.addRoom = function(lobbyName){
   $("#roomSelect").append("<div>" + lobbyName + "</div>");
 };
 
+app.addFriend = function(friend){
+  // var friend = event.data.friendName;
+  if (!user.friendsList[friend]) {
+    user.friendsList[friend] = friend;
+  }
+//add class method
 
-app.addFriend = function(){};
-app.handleSubmit = function(){};
+};
+app.handleSubmit = function(){
+  //build message to send
+  var message = {
+    username: user.username,
+    text: $("input.sendText").val(),
+    room: user.room
+  };
+
+  //pass built message to send
+  app.send(message);
+};
+
+
+app.init();
